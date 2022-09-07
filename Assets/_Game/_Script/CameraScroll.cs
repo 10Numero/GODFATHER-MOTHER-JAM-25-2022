9bbe15a816f2153a38296c998f2dc61dcb0ed298
Franci-Algera <algera.franci@gmail.com>
2022-09-07 10:49:24 +0200

    Scroll level

---
+++ Assets/_Game/_Script/CameraScroll.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScroll : MonoBehaviour
+{
+    [SerializeField] private Transform startPos;
+    [SerializeField] private Transform endPos;
+    [SerializeField] private AnimationCurve posOverTime;
+    [SerializeField] private float animTime;
+    private float timer = 0;
+    Transform cameraPos;
+
+    void Start()
+    {
+        cameraPos = Camera.main.transform; 
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        cameraPos.position = Vector2.Lerp(startPos.position, endPos.position, posOverTime.Evaluate(timer /animTime));
+    }
+}
