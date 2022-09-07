bd46068235ca0fcc7dcd4b8d9446793b3a84075e
Franci-Algera <algera.franci@gmail.com>
2022-09-07 09:57:41 +0200

    Get levels from folder

--- Assets/_Game/_Script/LevelSelect.cs
+++ Assets/_Game/_Script/LevelSelect.cs
@@ -1,25 +1,40 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class LevelSelect : MonoBehaviour
 {
-    [SerializeField] private string[] sceneName;
+    [SerializeField] private List<string> sceneName;
     [SerializeField] private LevelSelectHandler levelButton;
+    [SerializeField] private string path;
 
     private void Start()
     {
         LoadButtons();
     }
 
+    [Button("Find Levels")]
     public void FindLevels()
     {
-
+        sceneName.Clear();
+        sceneName = new List<string>();
+        var fullPath = Application.dataPath  + path;
+        Debug.Log("fullPath: " + fullPath);
+        DirectoryInfo dirInfo = new DirectoryInfo(fullPath);
+        foreach (var file in dirInfo.GetFiles())
+        {
+            if (file.Name.Contains("Level") && !file.Name.Contains(".meta"))
+            {
+                sceneName.Add(file.Name.Substring(0 , file.Name.Length -6));
+            }
+        }
     }
 
     public void LoadButtons() 
     {
-        for (int i = 0; i < sceneName.Length; i++)
+        for (int i = 0; i < sceneName.Count; i++)
         {
             LevelSelectHandler level = Instantiate<LevelSelectHandler>(levelButton, transform);
             level.InitButton(sceneName[i]);
