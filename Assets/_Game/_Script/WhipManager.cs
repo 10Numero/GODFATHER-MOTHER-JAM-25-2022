11a711a92665cfd0aad59f883a26e5c82114a8b6
Franci-Algera <algera.franci@gmail.com>
2022-09-07 14:57:14 +0200

    Sound integrations whip

--- Assets/_Game/_Script/WhipManager.cs
+++ Assets/_Game/_Script/WhipManager.cs
@@ -6,12 +6,20 @@ public class WhipManager : MonoBehaviour
 {
     private Vector2 direction;
     [SerializeField] private LineRenderer lineRenderer;
-    private bool hasInput = false;
     [SerializeField] private float whipTime = 1f;
+    private bool hasInput = false;
     private float timer = 0;
 
     private Vector2 targetPos;
     private RaycastHit2D _raycastHit;
+
+    [Header("Sound")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip whipLaunchOvenClip;
+    [SerializeField] private AudioClip whipLaunchSugerClip;
+    [SerializeField] private AudioClip whipHitOvenClip;
+    [SerializeField] private AudioClip whipHitSugerClip;
+    [SerializeField] private AudioClip whipHitJellyClip;
 
     private void Update()
     {
@@ -52,28 +60,63 @@ public class WhipManager : MonoBehaviour
         if (hasInput)
         {
             _raycastHit = Physics2D.Raycast(transform.position, direction);
-            Debug.DrawLine(transform.position, transform.position + (Vector3)direction, Color.red, 5);
             if (_raycastHit.collider != null)
             {
-                Debug.Log(_raycastHit.collider.name);
                 targetPos = _raycastHit.transform.position;
                 lineRenderer.SetPosition(0, transform.position);
                 lineRenderer.SetPosition(1, transform.position);
                 lineRenderer.enabled = true;
             }
             timer = whipTime;
+            var cube = _raycastHit.collider.transform.gameObject.GetComponent<ACube>();
+            switch (cube.cubeType)
+            {
+                case ACube.eCubeType.Oven:
+                    audioSource.clip = whipLaunchOvenClip;
+                    break;
+                case ACube.eCubeType.Sugar:
+                case ACube.eCubeType.Jelly:
+                    audioSource.clip = whipLaunchSugerClip;
+                    break;
+                case ACube.eCubeType.Caramel:
+                    break;
+                default:
+                    break;
+            }
+            audioSource.Play();
+            Debug.Log(cube.cubeType);
+
         }
 
     }
 
     private void WhipHit()
     {
+       
         var cube = _raycastHit.collider.transform.gameObject.GetComponent<ACube>();
 
         if (cube)
         {
             cube.Action();
         }
+
+        switch (cube.cubeType)
+        {
+            case ACube.eCubeType.Oven:
+                audioSource.clip = whipHitOvenClip;
+                break;
+            case ACube.eCubeType.Sugar:
+                audioSource.clip = whipHitSugerClip;
+                break;
+            case ACube.eCubeType.Jelly:
+                audioSource.clip = whipHitJellyClip;
+                break;
+            case ACube.eCubeType.Caramel:
+                break;
+            default:
+                break;
+        }
+        audioSource.Play();
     }
 
 }
