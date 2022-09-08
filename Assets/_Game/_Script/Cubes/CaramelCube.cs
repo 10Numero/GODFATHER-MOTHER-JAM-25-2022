d476cb516ff4a56a45a2e92195de3fa25e27c175
Zyrgal <Valud64@hotmail.fr>
2022-09-08 23:03:29 +0200

    Many level settings and camera prefab

--- Assets/_Game/_Script/Cubes/CaramelCube.cs
+++ Assets/_Game/_Script/Cubes/CaramelCube.cs
@@ -5,7 +5,9 @@ using Sirenix.OdinInspector;
 
 public class CaramelCube : MonoBehaviour
 {
+    [SerializeField] BoxCollider2D[] childTriggers;
 
+    private enum PlayerPosition { Top, right, Left, Down };
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -15,10 +17,55 @@ public class CaramelCube : MonoBehaviour
             whip.StopAllCoroutines();
             whip.EndAnimation();
             ACube cube = other.GetComponent<Player>().lastHittedCube;
-            other.transform.position = this.transform.position;
-            cube.CancelAction();
+
+            /*switch (PlayerPosition)
+            {
+                case PlayerPosition.Top:
+
+                default:
+            }*/
+
             
-        }else if(other.GetComponent<SugarCube>() != null)
+
+            //other.transform.position = this.transform.position;
+            cube.CancelAction();
+
+            Debug.Log(transform.position);
+
+            if (other.transform.position.y > transform.position.y) // En haut
+            {
+                Debug.Log("a");
+                other.transform.position = new Vector3(transform.position.x,
+                                                        other.transform.position.y,
+                                                        other.transform.position.z);
+            }
+            else if (other.transform.position.x > transform.position.x) // A droite
+            {
+                Debug.Log("b");
+
+                other.transform.position = new Vector3(transform.position.x + 1,
+                                                        transform.position.y,
+                                                        other.transform.position.z);
+            }
+            else if (other.transform.position.y < transform.position.y) // A gauche
+            {
+                Debug.Log("c");
+
+                other.transform.position = new Vector3(transform.position.x - 1,
+                                                        other.transform.position.y,
+                                                        other.transform.position.z);
+            }
+            else if (other.transform.position.x < transform.position.x) // En bas
+            {
+                Debug.Log("d");
+
+                other.transform.position = new Vector3(transform.position.x,
+                                                        other.transform.position.y - 1,
+                                                        other.transform.position.z);
+            }
+
+        }
+        else if(other.GetComponent<SugarCube>() != null)
         {
             ACube cube = other.GetComponent<SugarCube>();
             cube.CancelAction();
