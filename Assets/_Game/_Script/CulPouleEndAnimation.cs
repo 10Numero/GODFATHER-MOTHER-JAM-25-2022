2a1a736747efeb633e0aa56be66101a3354894c7
FlorianGOZARD <florian.gozard@wearexone.com>
2022-09-09 11:40:44 +0200

    cul poule end animation

---
+++ Assets/_Game/_Script/CulPouleEndAnimation.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CulPouleEndAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator _culPouleAnimator;
+    [SerializeField] private Transform _egg;
+    private void Awake()
+    {
+        _culPouleAnimator.enabled = false;
+    }
+
+    void Update()
+    {
+        var playerPos = Player.Instance.transform.position;
+        playerPos = new Vector3(Mathf.RoundToInt(playerPos.x), Mathf.RoundToInt(playerPos.y),
+            Mathf.RoundToInt(playerPos.z));
+
+        var selfPos = transform.position;
+        selfPos = new Vector3(Mathf.RoundToInt(selfPos.x), Mathf.RoundToInt(selfPos.y), Mathf.RoundToInt(selfPos.z));
+
+        if (playerPos == selfPos)
+        {
+            // _egg.gameObject.SetActive(false);
+            _culPouleAnimator.enabled = true;
+        }
+    }
+}
